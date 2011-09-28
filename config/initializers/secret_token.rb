24a3530415c8a65d651cc462b8ef22ae9b37a7f3
Day Davis Waterbury <day@azimuthonline.com>
2011-09-28 16:57:35 -0700

    Initial Commit: Ruby 1.9.2p290 and Rails 3.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metalyzer::Application.config.secret_token = '563fd2a5c29562f08319092198a12874931dd03eca3510d78ac02a24021d556c39f9fc8d740d05ed418598187619fb9792f2c62da9f50f34efcb6ae3aebb843b'
